c2da820a8952ad66ed04e2e53fc864d77270ee14
Francisco Sanchez <frango9000@gmail.com>
2021-06-16 22:49:12 +0200

    chore(project): add test containers

--- apps/chess-backend/src/test/java/dev/kurama/chess/backend/ChessBackendApplicationTests.java
+++ apps/chess-backend/src/test/java/dev/kurama/chess/backend/ChessBackendApplicationTests.java
@@ -2,8 +2,10 @@ package dev.kurama.chess.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "integration-test")
 class ChessBackendApplicationTests {
 
   @Test
