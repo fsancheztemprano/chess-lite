2cc78121851172377f561992e194d2c75c1448d3
Francisco Sanchez <frango9000@gmail.com>
2021-07-17 17:31:25 +0200

    feat(UM-13): init administration module

---
+++ apps/chess-app/src/app/main/modules/administration/administration.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AdministrationRoutingModule } from './administration-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, AdministrationRoutingModule],
+})
+export class AdministrationModule {}
