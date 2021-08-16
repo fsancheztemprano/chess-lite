911348f5e402d11fe6e050eba76ba822d199adc6
Francisco Sanchez <frango9000@gmail.com>
2021-08-16 17:36:50 +0200

    fix(UM-25): fix locale picker

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -23,7 +23,7 @@ export function initializeApp(appInitService: AppInitializationService) {
 }
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
