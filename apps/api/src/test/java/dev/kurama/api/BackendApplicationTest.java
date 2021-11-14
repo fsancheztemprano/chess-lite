b5d50d4d1ca1a22508793f083c66834794e2806a
Francisco Sanchez <frango9000@gmail.com>
2021-11-14 14:34:46 +0100

    test(UM-91): report all java tests

--- apps/api/src/test/java/dev/kurama/api/BackendApplicationTest.java
+++ apps/api/src/test/java/dev/kurama/api/BackendApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles(value = "integration-test")
-class BackendApplicationTests {
+class BackendApplicationTest {
 
   @Test
   void contextLoads() {
