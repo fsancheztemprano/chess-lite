0dd7d07507b5b44429d269c36d2e1b06241e4308
Francisco Sanchez <frango9000@gmail.com>
2022-01-15 17:05:07 +0100

    chore(deps): upgrade nx and angular 13

--- apps/app/src/test-setup.ts
+++ apps/app/src/test-setup.ts
@@ -1,3 +1,5 @@
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import 'jest-preset-angular/setup-jest';
 
 const style = global.document.createElement('style');
@@ -7,3 +9,8 @@ style.innerHTML = `
   }
 `;
 global.document.head.appendChild(style);
+
+getTestBed().resetTestEnvironment();
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  teardown: { destroyAfterEach: false },
+});
