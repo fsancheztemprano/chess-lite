a3d9c5878f1961a48c6f6899021517c2476d6146
Francisco Sanchez <frango9000@gmail.com>
2022-02-07 21:22:57 +0100

    test(UM-91): test hatoas utils

---
+++ apps/api/src/test/java/dev/kurama/api/core/utility/HateoasUtilsTest.java
@@ -0,0 +1,23 @@
+package dev.kurama.api.core.utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpMethod;
+
+import static dev.kurama.api.core.hateoas.relations.HateoasRelations.DEFAULT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.hateoas.MediaTypes.HAL_FORMS_JSON;
+
+class HateoasUtilsTest {
+
+  @Test
+  void should_add_default_affordance_to_given_link() {
+    Link actual = HateoasUtils.withDefaultAffordance(Link.of("/api"));
+
+    assertThat(actual.getAffordances()).hasSize(1)
+                                       .extracting(affordance -> affordance.getAffordanceModel(HAL_FORMS_JSON))
+                                       .extracting("name", "httpMethod")
+                                       .anySatisfy(reqs -> assertThat(reqs.toList()).contains(DEFAULT, HttpMethod.HEAD));
+
+  }
+}
