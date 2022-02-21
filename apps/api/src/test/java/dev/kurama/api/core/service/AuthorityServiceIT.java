6d40f52632cd631902fc58bd64c89c0e9dfe5a64
Francisco Sanchez <frango9000@gmail.com>
2022-02-21 19:55:46 +0100

    test(UM-92): IT authority service

--- apps/api/src/test/java/dev/kurama/api/core/service/AuthorityServiceIT.java
+++ apps/api/src/test/java/dev/kurama/api/core/service/AuthorityServiceIT.java
@@ -1,11 +1,76 @@
 package dev.kurama.api.core.service;
 
-import dev.kurama.api.BackendApplication;
-import org.springframework.boot.test.context.SpringBootTest;
+import static com.google.common.collect.Sets.newHashSet;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+import dev.kurama.api.core.domain.Authority;
+import dev.kurama.api.core.repository.AuthorityRepository;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles(value = "data-jpa")
-@SpringBootTest(classes = {BackendApplication.class})
+@DataJpaTest(showSql = false)
+@AutoConfigureTestDatabase(replace = NONE)
+@Import(AuthorityService.class)
 class AuthorityServiceIT {
 
+  @Autowired
+  private AuthorityRepository authorityRepository;
+
+  @Autowired
+  private AuthorityService authorityService;
+
+  private Authority authority1;
+  private Authority authority2;
+  private Authority authority3;
+  private Authority authority4;
+
+
+  @BeforeEach
+  void setUp() {
+    authority1 = Authority.builder().setRandomUUID().name(randomAlphanumeric(6)).build();
+    authority2 = Authority.builder().setRandomUUID().name(randomAlphanumeric(6)).build();
+    authority3 = Authority.builder().setRandomUUID().name(randomAlphanumeric(6)).build();
+    authority4 = Authority.builder().setRandomUUID().name(randomAlphanumeric(6)).build();
+
+    authorityRepository.saveAllAndFlush(newHashSet(authority1, authority2, authority3, authority4));
+  }
+
+  @Test
+  void should_find_all_by_id() {
+    Set<Authority> actual = authorityService.findAllById(newHashSet(authority2.getId(), authority4.getId()));
+
+    assertThat(actual.size()).isEqualTo(2);
+    assertThat(actual.toArray()).extracting("id")
+      .containsExactlyInAnyOrderElementsOf(newHashSet(authority2.getId(), authority4.getId()));
+  }
+
+  @Test
+  void should_get_all() {
+    Page<Authority> actual = authorityService.getAllAuthorities(PageRequest.of(0, 20));
+
+    assertThat(actual.getContent().size()).isEqualTo(4);
+    assertThat(actual.getContent().toArray()).extracting("id")
+      .containsExactlyInAnyOrderElementsOf(
+        newHashSet(authority1.getId(), authority2.getId(), authority3.getId(), authority4.getId()));
+  }
+
+  @Test
+  void should_find_one_by_id() {
+    Optional<Authority> authorityById = authorityService.findAuthorityById(authority3.getId());
+
+    assertThat(authorityById).isPresent()
+      .hasValueSatisfying(authority -> assertThat(authority.getId()).isEqualTo(authority3.getId()));
+  }
 }
