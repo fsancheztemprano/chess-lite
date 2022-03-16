513f9e4e99e58c496c3f0137e07ff313a33e2f96
Francisco Sanchez <frango9000@gmail.com>
2022-03-16 19:36:50 +0100

    pact(UM-34): test pact support utils

--- libs/consumer-pact/src/utils/token.util.spec.ts
+++ libs/consumer-pact/src/utils/token.util.spec.ts
@@ -1,11 +1,20 @@
+import * as jwt from 'jsonwebtoken';
+import { JwtPayload } from 'jsonwebtoken';
 import { jwtToken } from './token.util';
 
 describe('Token Utils', () => {
   it('should create jwt token', () => {
-    const token = jwtToken({
+    const authenticatedUser = {
       user: { id: '51236253-7c7c-45e4-b7f5-304b3b66a43d', username: 'admin' },
       authorities: ['profile:read', 'profile:delete', 'profile:update'],
-    });
+    };
+    const token = jwtToken(authenticatedUser);
     expect(token).toBeTruthy();
+
+    const decodedToken: JwtPayload = <JwtPayload>jwt.verify(token, 'secret', { algorithms: ['HS512'] });
+
+    expect(decodedToken).toBeTruthy();
+    expect(decodedToken.user).toStrictEqual(authenticatedUser.user);
+    expect(decodedToken.authorities).toStrictEqual(authenticatedUser.authorities);
   });
 });
