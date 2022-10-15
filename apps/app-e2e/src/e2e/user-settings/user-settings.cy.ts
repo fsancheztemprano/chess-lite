25df625a06476afdeaf0c9c0a7c0a01c44f8a326
Francisco Sanchez <frango9000@gmail.com>
2022-10-15 11:31:28 +0200

    e2e(UM-156): enhance user settings e2e

--- apps/app-e2e/src/e2e/user-settings/user-settings.cy.ts
+++ apps/app-e2e/src/e2e/user-settings/user-settings.cy.ts
@@ -26,6 +26,7 @@ describe('User Settings Module >', () => {
   });
 
   it('should edit user profile', () => {
+    cy.interceptApi('PATCH', '/user/profile').as('updateProfile');
     cy.get('[data-cy="user-profile-tile"]').click();
     cy.get('#mat-input-0').should('have.value', 'e2e-user1');
     cy.get('#mat-input-1').should('have.value', 'e2e-user1@localhost');
@@ -34,11 +35,13 @@ describe('User Settings Module >', () => {
     cy.get('#mat-input-3').should('have.value', '');
     cy.get('#mat-input-3').type('new last name');
     cy.get('[data-cy="update-profile-button"]').click();
+    cy.wait('@updateProfile').its('response.statusCode').should('eq', 200);
     cy.get('#toast-container').should('contain', 'User Profile Saved Successfully.');
     cy.get('[data-cy="nav-username"]').should('contain', 'new username');
   });
 
   it('should upload user avatar', () => {
+    cy.interceptApi('PATCH', '/user/profile/avatar').as('updateAvatar');
     cy.get('[data-cy="user-avatar-tile"]').click();
     cy.get('.mat-card-content > .mat-icon').should('contain', 'person_outline');
 
@@ -49,11 +52,13 @@ describe('User Settings Module >', () => {
       },
       { force: true },
     );
+    cy.wait('@updateAvatar').its('response.statusCode').should('eq', 200);
 
     cy.get('img[alt="Avatar"]').should('exist');
   });
 
   it('should change user password', () => {
+    cy.interceptApi('PATCH', '/user/profile/password').as('updatePassword');
     cy.get('[data-cy="user-password-tile"]').click();
     cy.get('#mat-input-0').type('e2e-user1');
     cy.get('#mat-input-1').type('e2e-user1');
@@ -61,6 +66,7 @@ describe('User Settings Module >', () => {
     cy.get('#mat-input-3').type('e2e-user1-new');
 
     cy.get('[data-cy="change-password-button"]').click();
+    cy.wait('@updatePassword').its('response.statusCode').should('eq', 200);
     cy.get('#toast-container').should('contain', 'Password changed successfully');
     cy.get('[data-cy="logout-menu-option"]').click();
     cy.get('app-home [data-cy="login-tile"]').click();
@@ -71,20 +77,24 @@ describe('User Settings Module >', () => {
   });
 
   it('should remove account', () => {
+    cy.interceptApi('DELETE', '/user/profile').as('deleteAccount');
     cy.get('[data-cy="user-remove-tile"]').click();
     cy.get('[data-cy="remove-account-button"]').click();
     cy.get('#mat-input-0').type('e2e-user1');
     cy.get('[data-cy="password-confirm-button"]').click();
+    cy.wait('@deleteAccount').its('response.statusCode').should('eq', 204);
     cy.get('#toast-container').should('contain', 'Account and all associated data was removed.');
     cy.get('[data-cy="login-menu-option"]').should('exist');
   });
 
   it('should change user preferences', () => {
+    cy.interceptApi('PATCH', '/user/profile/preferences').as('updateProfilePreferences');
     cy.get('[data-cy="user-preferences-tile"]').click();
     cy.get('[data-cy="preferences-dark-toggle"]').click();
     cy.get('#mat-select-value-1').click();
     cy.get('mat-option').contains('Spanish').click();
     cy.get('[data-cy="change-preferences-button"]').click();
+    cy.wait('@updateProfilePreferences').its('response.statusCode').should('eq', 200);
     cy.get('[data-cy="change-preferences-button"]').should('contain', 'Guardar y Aplicar');
   });
 });
