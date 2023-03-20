495fd622502d9dec13e8235e49a2d62b141c2014
Francisco Javier Sanchez Temprano <frango9000@gmail.com>
2023-03-20 22:29:11 +0100

    feat(UM-168): fix ttt board dark mode

--- libs/ui/feature/tic-tac-toe/src/lib/modules/game/services/tic-tac-toe-game.service.ts
+++ libs/ui/feature/tic-tac-toe/src/lib/modules/game/services/tic-tac-toe-game.service.ts
@@ -1,11 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { MessageService } from '@app/ui/shared/app';
-import {
-  ITicTacToeGameMove,
-  TicTacToeGame,
-  TicTacToeGameChangedMessage,
-  TicTacToeGameStatus,
-} from '@app/ui/shared/domain';
+import { ITicTacToeGameMove, TicTacToeGame, TicTacToeGameChangedMessage } from '@app/ui/shared/domain';
 import { Resource } from '@hal-form-client';
 import { BehaviorSubject, distinctUntilChanged, iif, map, Observable, of, shareReplay, switchMap, tap } from 'rxjs';
 
@@ -18,24 +13,6 @@ export class TicTacToeGameService {
   private readonly _game$: BehaviorSubject<TicTacToeGame | null> = new BehaviorSubject<TicTacToeGame | null>(null);
   private readonly _moves$: Observable<ITicTacToeGameMove[]> = this._game$.pipe(
     switchMap((game) => game?.getMoves() || []),
-    shareReplay(1),
-  );
-  private readonly _gameUi$: Observable<TicTacToeGame | null> = this._game$.pipe(
-    tap((game) => {
-      if (game) {
-        game.board$ = game.board?.split('').map((value, index) => {
-          const row = (index % 3) + 1;
-          const column = String.fromCharCode(65 + Math.floor(index / 3));
-          return {
-            value: value,
-            cell: `${column}${row}`,
-            canMove:
-              game.status === TicTacToeGameStatus.IN_PROGRESS && value === '_' && game.canMove(`${column}${row}`),
-            move: () => game.move(`${column}${row}`).subscribe(),
-          };
-        });
-      }
-    }),
     shareReplay(1),
   );
 
@@ -67,7 +44,7 @@ export class TicTacToeGameService {
   }
 
   get game$(): Observable<TicTacToeGame | null> {
-    return this._gameUi$;
+    return this._game$;
   }
 
   get moves$(): Observable<ITicTacToeGameMove[]> {
