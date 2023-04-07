7fb06cd165933992076aae08bff363728f40e630
Francisco Javier Sanchez Temprano <frango9000@gmail.com>
2023-04-07 18:31:02 +0200

    feat(UM-168): e2e test tic tac toe game

--- apps/app-e2e/src/e2e/tic-tac-toe/tic-tac-toe-game.cy.ts
+++ apps/app-e2e/src/e2e/tic-tac-toe/tic-tac-toe-game.cy.ts
@@ -1,3 +1,5 @@
+import { Interception } from 'cypress/types/net-stubbing';
+
 describe('Tic Tac Toe Game', () => {
   beforeEach(() => {
     cy.setState(2).clearLocalStorage();
@@ -14,13 +16,39 @@ describe('Tic Tac Toe Game', () => {
           playerXUsername: 'e2e-user4',
           isPrivate: true,
         },
-      }).then((xhr) => cy.visit(`/tic-tac-toe/games/${xhr.body.id}`));
+      }).then((xhr) => {
+        cy.interceptApi('GET', `/tic-tac-toe/game/${xhr.body.id}`).as('getGame');
+        cy.interceptApi('POST', `/tic-tac-toe/game/${xhr.body.id}/move`).as('move');
+        cy.visit(`/tic-tac-toe/games/${xhr.body.id}`);
+      });
     });
 
-    it.only('should play a complete game', () => {
-      cy.wait(10000);
+    it('should play a complete game', () => {
+      cy.get('[data-cy="game-pending"]').within(() => cy.get('.right-button').click());
+
+      clickCell('A1');
+      clickCell('B1');
+      clickCell('C1');
+      clickCell('C2');
+      clickCell('C3');
+      clickCell('B3');
+      clickCell('A3');
+      clickCell('A2');
+      clickCell('B2');
+      cy.wait('@getGame');
+      cy.get<Interception>('@getGame.11').its('response.body.status').should('eq', 'FINISHED');
     });
 
-    it('should reject game', () => {});
+    it('should reject game', () => {
+      cy.get('[data-cy="game-pending"]').within(() => {
+        cy.get('.left-button').click();
+      });
+      cy.get('[data-cy="game-rejected"]');
+    });
   });
+
+  function clickCell(cell: string): void {
+    cy.get(`.cell.cell-${cell} .new-move-icon`).invoke('show').click();
+    cy.wait('@move').its('response.statusCode').should('eq', 201);
+  }
 });
