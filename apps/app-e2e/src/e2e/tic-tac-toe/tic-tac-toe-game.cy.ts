ddeb7e9c3bd3fc1bfceb9d7da12c91b9ca6c0620
Francisco Javier Sanchez Temprano <frango9000@gmail.com>
2023-04-08 15:19:52 +0200

    feat(UM-168): e2e test tic tac toe game

--- apps/app-e2e/src/e2e/tic-tac-toe/tic-tac-toe-game.cy.ts
+++ apps/app-e2e/src/e2e/tic-tac-toe/tic-tac-toe-game.cy.ts
@@ -1,7 +1,10 @@
 import { Interception } from 'cypress/types/net-stubbing';
 
 describe('Tic Tac Toe Game', () => {
+  let gameId: string;
+
   beforeEach(() => {
+    Cypress.session.clearAllSavedSessions();
     cy.setState(2).clearLocalStorage();
   });
 
@@ -16,10 +19,11 @@ describe('Tic Tac Toe Game', () => {
           playerXUsername: 'e2e-user4',
           isPrivate: true,
         },
-      }).then((xhr) => {
-        cy.interceptApi('GET', `/tic-tac-toe/game/${xhr.body.id}`).as('getGame');
-        cy.interceptApi('POST', `/tic-tac-toe/game/${xhr.body.id}/move`).as('move');
-        cy.visit(`/tic-tac-toe/games/${xhr.body.id}`);
+      }).then(({ body }) => {
+        gameId = body.id;
+        cy.interceptApi('POST', `/tic-tac-toe/game/${gameId}/move`).as('move');
+        cy.interceptApi('GET', `/tic-tac-toe/game/${gameId}`).as('getGame');
+        visitGame();
       });
     });
 
@@ -40,12 +44,72 @@ describe('Tic Tac Toe Game', () => {
     });
 
     it('should reject game', () => {
-      cy.get('[data-cy="game-pending"]').within(() => {
-        cy.get('.left-button').click();
-      });
+      cy.get('[data-cy="game-pending"]').within(() => cy.get('.left-button').click());
+      visitGame();
       cy.get('[data-cy="game-rejected"]');
     });
   });
+
+  describe('as players', () => {
+    function session(username: string, password?: string) {
+      return cy.session(username, () => cy.login(username, password || ''));
+    }
+
+    function clickCellAsPlayer(cell: string, player: string): void {
+      session(player);
+      visitGame();
+      clickCell(cell);
+    }
+
+    beforeEach(() => {
+      session('e2e-user3', 'e2e-user3');
+      cy.requestApi({
+        method: 'POST',
+        url: '/tic-tac-toe/game',
+        body: {
+          playerOUsername: 'e2e-user4',
+          isPrivate: true,
+        },
+      }).then(({ body }) => {
+        gameId = body.id;
+        cy.interceptApi('POST', `/tic-tac-toe/game/${gameId}/move`).as('move');
+      });
+    });
+
+    it('should play a complete game', () => {
+      session('e2e-user4', 'e2e-user4');
+      visitGame();
+      cy.get('[data-cy="game-pending"]').within(() => cy.get('.right-button').click());
+
+      clickCellAsPlayer('A1', 'e2e-user3');
+      clickCellAsPlayer('A2', 'e2e-user4');
+      clickCellAsPlayer('A3', 'e2e-user3');
+      clickCellAsPlayer('B3', 'e2e-user4');
+      clickCellAsPlayer('C3', 'e2e-user3');
+      clickCellAsPlayer('C2', 'e2e-user4');
+      clickCellAsPlayer('C1', 'e2e-user3');
+      clickCellAsPlayer('B1', 'e2e-user4');
+      clickCellAsPlayer('B2', 'e2e-user3');
+
+      cy.interceptApi('GET', `/tic-tac-toe/game/${gameId}`).as('getGame');
+      session('e2e-user4', 'e2e-user4');
+      visitGame();
+      cy.wait('@getGame').its('response.body.status').should('eq', 'FINISHED');
+    });
+
+    it('should reject game', () => {
+      session('e2e-user4', 'e2e-user4');
+      visitGame();
+
+      cy.get('[data-cy="game-pending"]').within(() => cy.get('.left-button').click());
+      visitGame();
+      cy.get('[data-cy="game-rejected"]');
+    });
+  });
+
+  function visitGame() {
+    return cy.visit(`/tic-tac-toe/games/${gameId}`);
+  }
 
   function clickCell(cell: string): void {
     cy.get(`.cell.cell-${cell} .new-move-icon`).invoke('show').click();
