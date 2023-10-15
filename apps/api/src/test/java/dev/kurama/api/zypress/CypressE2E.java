5d193901b78a5e741fe8a277b027510794392365
Francisco Sanchez <frango9000@gmail.com>
2023-10-15 17:50:58 +0200

    chore(deps): update to cypress 13.3.1

--- apps/api/src/test/java/dev/kurama/api/zypress/CypressE2E.java
+++ apps/api/src/test/java/dev/kurama/api/zypress/CypressE2E.java
@@ -139,7 +139,7 @@ class CypressE2E {
   }
 
   private GenericContainer createCypressContainer(CountDownLatch countDownLatch, String browser, String specPattern) {
-    GenericContainer genericContainer = new GenericContainer<>("cypress/included:12.10.0")
+    GenericContainer genericContainer = new GenericContainer<>("cypress/included:13.3.1")
       //
       .withCommand("--browser", !isEmpty(browser) ? browser : "electron")
       .withAccessToHost(true)
